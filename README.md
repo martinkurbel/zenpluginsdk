e8888e8a6da54ace08bf19d9f94a43dd7b1da6c1
Martin Kurbel <martin@kurbel.sk>
2021-02-10 12:01:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# zenpluginsdk
---
+++ ZENPluginSDK.xcframework/ios-arm64/ZENPluginSDK.framework/Headers/ZENPluginSDK.h
@@ -0,0 +1,18 @@
+//
+//  ZENPluginSDK.h
+//  ZENPluginSDK
+//
+//  Created by Martin Kurbel on 22/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZENPluginSDK.
+FOUNDATION_EXPORT double ZENPluginSDKVersionNumber;
+
+//! Project version string for ZENPluginSDK.
+FOUNDATION_EXPORT const unsigned char ZENPluginSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZENPluginSDK/PublicHeader.h>
+
+
